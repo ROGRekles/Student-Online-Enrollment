3510ccb56641563fe97812ad89e19692d67a6cce
Tony Han <57244611+hansangyeob@users.noreply.github.com>
2021-05-13 18:21:08 +0700

    edit profile design / history table

--- enrolment/student.php
+++ enrolment/student.php
@@ -66,7 +66,7 @@ if (isset($_POST['logout'])) {
 
 	<link href="../enrolment/css/main.css" rel="stylesheet" />
 	<link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Open+Sans" />
-    <!-- CSS only -->
+	<!-- CSS only -->
 
 	<title>OES Student Profile</title>
 
@@ -74,37 +74,37 @@ if (isset($_POST['logout'])) {
 	<table>
 		<tr>
 			<td>
-                <a href="../enrolment/home.php" style="cursor:pointer;">
-                    <img src="image/logo.jpg" alt="logo" class="logo"> 
-                </a>    
-            </td>
-        	<td>
-                <a href="../enrolment/home.php" style="cursor:pointer;text-decoration: none;">
-                    <h3 class="header">Online Enrolment System</h3>
-                </a>
-            </td>
+				<a href="../enrolment/home.php" style="cursor:pointer;">
+					<img src="image/logo.jpg" alt="logo" class="logo">
+				</a>
+			</td>
+			<td>
+				<a href="../enrolment/home.php" style="cursor:pointer;text-decoration: none;">
+					<h3 class="header">Online Enrolment System</h3>
+				</a>
+			</td>
 
-			<div style = "background-color:#f5f5f5; height:35px;">
-                    <form action="#" method="POST" >
-                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                            <input type="submit" name="logout" value="Log out" class="logout-btn"  style = "background-color:#F5F5F5;">
-                    </form>
-                    <b style=" font-size:small; padding-top:10px; float:right; background-color:#F5F5F5;"><?php echo "Welcome, " . $user ." ". "  "."|"?></b>
-            </div>
+			<div style="background-color:#f5f5f5; height:35px;">
+				<form action="#" method="POST">
+					&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+					&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+					&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+					<input type="submit" name="logout" value="Log out" class="logout-btn" style="background-color:#F5F5F5;">
+				</form>
+				<b style=" font-size:small; padding-top:10px; float:right; background-color:#F5F5F5;"><?php echo "Welcome, " . $user . " " . "  " . "|" ?></b>
+			</div>
 		</tr>
 	</table>
 	<hr>
 
 	<div class="nagivate-bar">
 		<div class="menu-bar">
-		<ul>
-                    <li><a href="../enrolment/home.php">Enrolment</a></li>
-                    <li><a href="../enrolment/programmap.php">Program map</a></li>
-                    <li><a href="../enrolment/student.php">Profile</a></li>
-                    <li><a href="../enrolment/help.php">Help</a></li>
-                </ul>
+			<ul>
+				<li><a href="../enrolment/home.php">Enrolment</a></li>
+				<li><a href="../enrolment/programmap.php">Program map</a></li>
+				<li><a href="../enrolment/student.php">Profile</a></li>
+				<li><a href="../enrolment/help.php">Help</a></li>
+			</ul>
 		</div>
 	</div>
 
@@ -114,53 +114,87 @@ if (isset($_POST['logout'])) {
 
 <body>
 
-<div class="table-responsive-sm">
-	<div class="row justify-content-center">
-	<div>
-		<div class="user-img">
-			<img src="../enrolment/image/user_img.png" alt="user_pic" >
+	<div >
+		<div class="user_infomation_text">
+		<!-- <h3>Personal Information</h3>
+		 -->
+			<div class="user-img">
+				<img src="../enrolment/image/user_img.png" alt="user_pic">
+			</div>
+
+
+			<div class="user-info">
+				<table>
+					<tr>
+						<td>Name :</td>
+						<td style="font-weight: normal;"><?= $name; ?></td>
+					</tr>
+					<tr>
+						<td>Student ID :</td>
+						<td style="font-weight: normal;"><?= $rmit_student_id; ?></td>
+					</tr>
+					<tr>
+						<td>Gender :</td>
+						<td style="font-weight: normal;"><?= $gender; ?></td>
+					</tr>
+					<tr>
+						<td>Date of Birth :</td>
+						<td style="font-weight: normal;"><?= $dob; ?></td>
+					</tr>
+					<tr>
+						<td>Major :</td>
+						<td style="font-weight: normal;"><?= $major; ?></td>
+					</tr>
+					<tr>
+						<td>GPA : </td>
+						<td style="font-weight: normal;"><?= $gpa; ?></td>
+					</tr>
+					<tr>
+						<td>Total Credit :</td>
+						<td style="font-weight: normal;"><?= $creditpoint; ?></td>
+					</tr>
+					<tr>
+						<td>Campus :</td>
+						<td style="font-weight: normal;"><?= $campus; ?></td>
+					</tr>
+
+				</table>
+
+			</div>
 		</div>
+	</div>
 
-		<div class="user-info">
-			<table>
-				<tr>
-					<td>Name :</td>
-					<td style="font-weight: normal;"><?= $name; ?></td>
-				</tr>
-				<tr>
-					<td>Student ID :</td>
-					<td style="font-weight: normal;"><?= $rmit_student_id; ?></td>
-				</tr>
-				<tr>
-					<td>Gender :</td>
-					<td style="font-weight: normal;"><?= $gender; ?></td>
-				</tr>
-				<tr>
-					<td>Date of Birth :</td>
-					<td style="font-weight: normal;"><?= $dob; ?></td>
-				</tr>
-				<tr>
-					<td>Major :</td>
-					<td style="font-weight: normal;"><?= $major; ?></td>
-				</tr>
-				<tr>
-					<td>GPA : </td>
-					<td style="font-weight: normal;"><?= $gpa; ?></td>
-				</tr>
-				<tr>
-					<td>Total Credit :</td>
-					<td style="font-weight: normal;"><?= $creditpoint; ?></td>
-				</tr>
-				<tr>
-					<td>Campus :</td>
-					<td style="font-weight: normal;"><?= $campus; ?></td>
-				</tr>
-
-			</table>
-
-		</div>
-
-
+	<div class="history_table">
+		<h3>Course history</h3>
+				<table class="history">
+					<thead>
+						<th>No.</th>
+						<th>Course code</th>
+						<th>Course title</th>
+						<th>Status</th>
+						<th>Semester</th>
+						<th>Year</th>
+					</thead>
+					<tbody>
+						<tr>
+							<td>1</td>
+							<td>COSC0001</td>
+							<td>Introduction to IT</td>
+							<td>In progress</td>
+							<td>Feb</td>
+							<td>2021</td>
+						</tr>
+						<tr>
+							<td>2</td>
+							<td>COSC0002</td>
+							<TD>Introduction to Programming</TD>
+							<td>In progress</td>
+							<td>Feb</td>
+							<td>2021</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
 </body>
 
 
@@ -168,7 +202,7 @@ if (isset($_POST['logout'])) {
 <footer class="footer">
 
 	<div>
-	<p>Address: 702 Nguyen Van Linh, Tan Hung, Quan 7, Ho Chi Minh City</p>
+		<p>Address: 702 Nguyen Van Linh, Tan Hung, Quan 7, Ho Chi Minh City</p>
 		<p>Hotline: +84) 1123 4435. All RIGHTS RESERVED.</p>
 	</div>
 
